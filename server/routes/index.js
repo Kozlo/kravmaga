b9c8dc73147c46f904c61f52bbfa616e628805fb
Martins Kozlovskis <mkozlovskis@gmail.com>
2017-02-03 09:26:13 +0200

    fixed group model and routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 const authRoutes = require('./auth');
 const usersRoutes = require('./users');
+const groupRoutes = require('./groups');
 const { getAll } = require('../controllers/');
 
 /**
@@ -14,6 +15,7 @@ const { getAll } = require('../controllers/');
  */
 router.use('/', authRoutes);
 router.use('/users', usersRoutes);
+router.use('/groups', groupRoutes);
 
 /**
  * Route for unrecognized get requests.
