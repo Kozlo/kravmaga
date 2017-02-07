85447626206cd0c7f7acbb5f55ac2e0abf1c8bbb
Martins Kozlovskis <mkozlovskis@gmail.com>
2017-02-07 12:46:26 +0200

    added lessons API

---
+++ server/controllers/lessons.js
@@ -0,0 +1,111 @@
+/**
+ * Lessons controller.
+ */
+
+const config = require('../config');
+const helpers = require('../helpers');
+const Lesson = require('../models/lesson');
+
+const { httpStatusCodes } = config;
+
+module.exports = {
+
+    /**
+     * Create an entry based on the passed params.
+     *
+     * @public
+     * @param {Object} req Request object
+     * @param {Object} res Response object
+     * @param {Function} next Executes the next matching route
+     */
+    createOne(req, res, next) {
+        const entryProps = req.body;
+
+        Lesson.create(entryProps)
+            .then(entry => res.status(httpStatusCodes.created).send(entry))
+            .catch(err => {
+                const entryExistsError = helpers.entryExistsError(err);
+
+                if (entryExistsError) return next(entryExistsError);
+
+                next(err);
+            });
+    },
+
+    /**
+     * Retrieves all entries.
+     *
+     * @public
+     * @param {Object} req Request object
+     * @param {Object} res Response object
+     * @param {Function} next Executes the next matching route
+     */
+    getAll(req, res, next) {
+        const filters = req.query.filters || {};
+        const sorters = req.query.sorters || { 'updatedAt': -1 };
+
+        Lesson.find(filters)
+            .then(entries => res.status(httpStatusCodes.ok).send(entries))
+            .catch(err => next(err));
+    },
+
+    /**
+     * Retrieves a single entry based on the ID.
+     *
+     * @public
+     * @param {Object} req Request object
+     * @param {Object} res Response object
+     * @param {Function} next Executes the next matching route
+     */
+    getOne(req, res, next) {
+        const entryId = req.params.id;
+
+        Lesson.findById(entryId)
+            .then(entry => res.status(httpStatusCodes.ok).send(entry))
+            .catch(err => next(err));
+    },
+
+    /**
+     * Updates the specified entry.
+     *
+     * @public
+     * @param {Object} req Request object
+     * @param {Object} res Response object
+     * @param {Function} next Executes the next matching route
+     */
+    updateOne(req, res, next) {
+        const entryId = req.params.id;
+        const entryProps = req.body;
+
+        if (typeof entryProps.attendees !== 'undefined' && !entryProps.attendees) {
+            entryProps.attendees = [];
+        }
+
+        Lesson.findByIdAndUpdate(entryId, entryProps, { 'new': true })
+            .then(updatedEntry => res.status(httpStatusCodes.ok).send(updatedEntry))
+            .catch(err => {
+                const entryExistsError = helpers.entryExistsError(err);
+
+                if (entryExistsError) return next(entryExistsError);
+
+                next(err);
+            });
+    },
+
+    /**
+     * Deletes the specified entry.
+     *
+     * @public
+     * @param {Object} req Request object
+     * @param {Object} res Response object
+     * @param {Function} next Method for further execution
+     */
+    deleteOne(req, res, next) {
+        const entryId = req.params.id;
+
+        Lesson.findByIdAndRemove(entryId)
+            .then(deletedEntry => res.status(httpStatusCodes.ok).send(deletedEntry))
+            .catch(err => next(err));
+    }
+
+};
