8f0d6c8c56f473a5d8881e71f7b8926288f69a35
Martins Kozlovskis <mkozlovskis@gmail.com>
2017-02-22 21:59:25 +0200

    fixe a profile view/login bug

--- app/components/user/Page.js
+++ app/components/user/Page.js
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import UserStore from '../../stores/UserStore';
-import UserActions from '../../actions/UserActions';
-
 import Page from '../shared/Page';
 import ProfilePanel from './profile/Panel';
 import LessonsPanel from './lessons/Panel';
